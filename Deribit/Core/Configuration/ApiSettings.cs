646eed3feac484e5e360fe59b31a89bc9c80c6f0
Jay Leimer <jay@jinxx.eu>
2021-02-25 14:26:37 +0100

    removed code

--- Deribit/Core/Configuration/ApiSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Deribit.Core.Configuration
-{
-    public static class ApiSettings
-    {
-        public static string JsonRpc { get; set; } = "2.0";
-    }
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-# DeribitClient
-Deribit C# Client Library <br/>
-Allows easy communication with Deribits API for trading Bitcoin
